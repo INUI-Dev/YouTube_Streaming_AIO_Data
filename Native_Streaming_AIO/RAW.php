1be8f45e5ea34652d436cbde07cacdb903baace2
Albdroid.AL <sxt@usa.com>
2020-12-09 16:20:06 +0100

    Update RAW.php

--- Native_Streaming_AIO/RAW.php
+++ Native_Streaming_AIO/RAW.php
@@ -18,12 +18,6 @@
  │ Created   │ 19 January 2020                                │
  │ Modified  │ 08 December 2020                               │
  └────────────────────────────────────────────────────────────┘
- [x] To Get Live Stream Required Live Stream Video ID
- Formats
- 96 = 1080P Full HD
- 95 = 720P HD
- 94 = 480P SD
- 93 = 360P Low
 */
 
 // FIXED 08 December 2020
@@ -97,43 +91,8 @@ function get_data($url) {
 }
 
 $url = get_data('https://www.youtube.com/watch?v=' . $id);
-
-// NEW
 preg_match('/"hlsManifestUrl":"(.*?)"/',$url,$match);
 $stream = $match[1];
-
-/*
-// AUTO MODE
-$hlsManifestUrl = '/,\\\\"hlsManifestUrl\\\\":\\\\"(.*?)\\\\"/m';
-preg_match_all($hlsManifestUrl, $url, $matches, PREG_PATTERN_ORDER, 0);
-// AUTO MODE
-
-// MANUAL MODE PUNON
-//preg_match_all($hlsManifestUrl, $url, $matches, PREG_SET_ORDER, 0);
-//preg_match_all('/,\\\\"hlsManifestUrl\\\\":\\\\"(.*?)\\\\"/m',$url,$matches, PREG_PATTERN_ORDER);
-// MANUAL MODE PUNON
-
-$var1=$matches[1][0];
-$var1=str_replace("\/", "/", $var1);
-$man = get_data($var1);
-/*
- QUALITY SETTINGS
- 96 = 1920x1080 
-$regex = '/(https:\/.*\/96\/.*index.m3u8)/U';
-
- 95 = 1280x720
-$regex = '/(https:\/.*\/95\/.*index.m3u8)/U';
-
- 94 = 854x480
-$regex = '/(https:\/.*\/94\/.*index.m3u8)/U';
-
- 93 = 640x360
-$regex = '/(https:\/.*\/93\/.*index.m3u8)/U';
-*/
-/*
-preg_match_all('/(https:\/.*\/95\/.*index.m3u8)/U',$man,$matches, PREG_PATTERN_ORDER);
-$stream = $matches[1][0];
-*/
 echo $stream;
 }
 ?>
